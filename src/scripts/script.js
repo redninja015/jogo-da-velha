ea2ebfbb24f7886fe4402d64d88df7346bf92f2c
redninja015 <152506590+redninja015@users.noreply.github.com>
2023-11-30 11:17:38 -0300

    Update script.js

--- src/scripts/script.js
+++ src/scripts/script.js
@@ -97,9 +97,9 @@ function changeWinner(square) {
 }
 
 function changesColorSquare(square1, square2, square3) {
-    square1.style.background = '#0f0';
-    square2.style.background = '#0f0';
-    square3.style.background = '#0f0';
+    square1.style.background = '#FF0000';
+    square2.style.background = '#FF0000';
+    square3.style.background = '#FF0000';
 }
 
 function checkSequence(square1, square2, square3) {
